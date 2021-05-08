1f5f507a982f986e864dd7cd9e69417bd708b565
Evan Gray <evanthegrayt@vivaldi.net>
2021-05-08 00:35:00 -0500

    Additions to Version

--- lib/standup_md/version.rb
+++ lib/standup_md/version.rb
@@ -5,23 +5,46 @@ module StandupMD
   # Module that contains all gem version information. Follows semantic
   # versioning. Read: https://semver.org/
   module Version
-
     ##
     # Major version.
+    #
+    # @return [Integer]
     MAJOR = 0
 
     ##
     # Minor version.
+    #
+    # @return [Integer]
     MINOR = 3
 
     ##
     # Patch version.
-    PATCH = 9
+    #
+    # @return [Integer]
+    PATCH = 10
+
+    ##
+    # Version as +[MAJOR, MINOR, PATCH]+
+    #
+    # @return [Array]
+    def self.to_a
+      [MAJOR, MINOR, PATCH]
+    end
 
     ##
     # Version as +MAJOR.MINOR.PATCH+
+    #
+    # @return [String]
     def self.to_s
-      "#{MAJOR}.#{MINOR}.#{PATCH}"
+      to_a.join('.')
+    end
+
+    ##
+    # Version as +{major: MAJOR, minor: MINOR, patch: PATCH}+
+    #
+    # @return [Hash]
+    def self.to_h
+      Hash[%i[major minor patch].zip(to_a)]
     end
   end
 end
--- test/standup_md/version_test.rb
+++ test/standup_md/version_test.rb
@@ -4,6 +4,28 @@ require_relative '../test_helper'
 require_relative '../../lib/standup_md'
 
 class TestVersion < TestHelper
+  def test_to_a
+    assert_equal(
+      [
+        StandupMD::Version::MAJOR,
+        StandupMD::Version::MINOR,
+        StandupMD::Version::PATCH
+      ],
+      StandupMD::Version.to_a
+    )
+  end
+
+  def test_to_h
+    assert_equal(
+      {
+        major: StandupMD::Version::MAJOR,
+        minor: StandupMD::Version::MINOR,
+        patch: StandupMD::Version::PATCH
+      },
+      StandupMD::Version.to_h
+    )
+  end
+
   def test_to_s
     assert_match(/\d+\.\d+.\d+/, StandupMD::Version.to_s)
   end
