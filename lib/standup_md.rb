cbe983c8fa1efbd6436f87789063f4febb2c9d43
Evan Gray <evanthegrayt@vivaldi.net>
2024-01-29 16:27:32 -0600

    Conform to standard and update CI test versions

--- lib/standup_md.rb
+++ lib/standup_md.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-require_relative 'standup_md/version'
-require_relative 'standup_md/file'
-require_relative 'standup_md/entry'
-require_relative 'standup_md/entry_list'
-require_relative 'standup_md/cli'
-require_relative 'standup_md/config'
+require_relative "standup_md/version"
+require_relative "standup_md/file"
+require_relative "standup_md/entry"
+require_relative "standup_md/entry_list"
+require_relative "standup_md/cli"
+require_relative "standup_md/config"
 
 ##
 # The main module for the gem. Provides access to configuration classes.
 module StandupMD
-  @config_file_loaded = false
-
   module_function
 
   ##
@@ -37,28 +35,5 @@ module StandupMD
   #   StandupMD.configure { |s| s.cli.editor = 'mate' }
   def configure
     yield config
-  end
-
-  ##
-  # Has a config file been loaded?
-  #
-  # @return [Boolean]
-  def config_file_loaded?
-    @config_file_loaded
-  end
-
-  ##
-  # Loads a config file.
-  #
-  # @param [String] file
-  #
-  # @return [String] file
-  def load_config_file(config_file)
-    ::File.expand_path(config_file).tap do |file|
-      raise "File #{file} does not exist." unless ::File.file?(file)
-
-      @config_file_loaded = true
-      load file
-    end
   end
 end
